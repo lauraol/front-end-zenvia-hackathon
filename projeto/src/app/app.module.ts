726c6db2c457455b054fe1de4445b6b3dd6116cd
Laura Oliveira Pedro <57760301+lauraol@users.noreply.github.com>
2020-09-20 20:00:48 -0300

    fix: adicionando forms module no app.module

--- projeto/src/app/app.module.ts
+++ projeto/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,7 +30,7 @@ import { UsuarioService } from './services/usuario.service';
     FooterComponent,
     TimeComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [UsuarioService],
   bootstrap: [AppComponent],
 })
